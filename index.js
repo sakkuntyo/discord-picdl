55324bef8ad13b04cc1e3625c317b49ef5a0fc7b
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2019-08-04 22:42:39 +0900

    おうむ返し実装

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if(msg.author.bot) return;
+  console.log(msg);
+  msg.channel.send(msg.content)
+});
+
+client.login('<token>');
