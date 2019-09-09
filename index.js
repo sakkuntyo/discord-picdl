525cb6d5cdd025ef23042ffc6498e1a1f48029b8
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2019-09-09 11:34:11 +0900

    dev

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,7 +9,10 @@ client.on('ready', () => {
 client.on('message', msg => {
   if(msg.author.bot) return;
   console.log(msg);
-  msg.channel.send(msg.content)
+  msg.attachments.forEach(a => {
+    fs.writeFileSync(`./${a.name}`,a.file);
+  })
+  //msg.channel.send(msg.content)
 });
 
 client.login('<token>');
